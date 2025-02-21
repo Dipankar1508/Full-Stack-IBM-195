4ba6c51ffc01ec25c6326b751ee2239551af87ec
Dipankar Sarkar <dipankarsarkar6297599215@gmail.com>
2025-02-21 15:46:48 +0530

    classwork

---
+++ Day 9/classwork/script.js
@@ -0,0 +1,25 @@
+{
+    let num = 11;
+    console.log(num);
+}
+// arithmetic : + , - , * , / , % , **
+// relational : == , === , != , !== , > , >= , < , <=
+// logical : && , || ,!
+// assignment : = , += , -= , *= , /=
+
+/*let a = 20;
+console.log(typeof a);
+let b = "10";
+console.log(typeof b);
+console.log(typeof +b);
+console.log((a += a));*/
+
+function sum(a, b) { // parameter
+    console.log(a + b);
+}
+let a = 10;
+let b = 20;
+sum(a, b); // argument
+
+console.log(name);
+var name = "Dipankar";
